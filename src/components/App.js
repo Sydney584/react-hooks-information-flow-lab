51333e484f6ef745f08c1d5aaab61cbff7d2b5aa
sydney <sydneychase5841@gmail.com>
2021-05-22 21:46:10 -0400

    updating assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
 
 function App() {
   const [items, setItems] = useState(itemData);
@@ -12,14 +13,10 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={items} />
-    </div>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+        
+       <ShoppingList items={items} />
+      </div>
   );
 }
 
